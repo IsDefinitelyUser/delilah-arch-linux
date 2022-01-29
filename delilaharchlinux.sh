a93a4056f260bfd62ecbc596e5f01296dde31193
Dana Delilah Burns <xmwx38@disroot.org>
2022-01-29 06:15:10 -0800

    updated delilaharchlinux.sh

--- delilaharchlinux.sh
+++ delilaharchlinux.sh
@@ -1,10 +1,14 @@
 # | Delilah Arch Linux | proot-distro | termux |
 # This is a custom Arch distribution plug-in.
 # It is meant for use with termux using proot-distro.
+# Included packages:
+# • git
+# • youtube-dl
+# ...and soon, there will be more!! :-) :-)
 
 DISTRO_NAME="Delilah Arch Linux"
 
-DISTRO_COMMENT="Customized Arch build for use with proot-distro on 	Termux."
+DISTRO_COMMENT="Customized Arch build for use with proot-distro on 	Termux. It includes commands such as: youtube-dl for streamimg (or whatever), git for cloning repos."
 
 TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v1.10.1/archlinux-aarch64-pd-v1.10.1.tar.xz"
 TARBALL_SHA256['aarch64']="0faef6f391a367424eec971ca256a2f980576283f6b67235eb04e47fefc90e0c"
@@ -20,14 +24,16 @@ TARBALL_SHA256['x86_64']="7bc126d4e13af10be7b9ff2ae3041dee532f8c7c978f82e8163df4
 # proot environment.
 
 distro_setup() {
-	echo
-	echo " |>| ""Delilah Arch Linux"" |<| "
-	echo " |>| ""proot-distro – termux"" |<| "
-	echo
-	echo " |>|>| ”"Package Manager”” |<|<| "
-	echo " |>|>| ""pacman"" |<|<| "
-	echo
+	echo && echo
+	echo " |«∆∆∆»| Delilah  Arch   Linux |«∆∆∆»| "
+	echo "  |»∆«|  proot-distro – termux  |»∆«| "
+	echo "    \                             / "
+	echo "  |«∆∆∆»|  Package   Manager  |«∆∆∆»| "
+	echo "   |»∆«|        pacman         |»∆«| "
+	echo && echo
 	run_proot_cmd pacman --noconfirm -Syu
 	run_proot_cmd pacman -Syu --ignore sudo base-devel
 	run_proot_cmd pacman -S --noconfirm git curl wget
+	run_proot_cmd curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+	run_proot_cmd chmod a+rx /usr/local/bin/youtube-dl
 }
